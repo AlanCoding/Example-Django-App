a5fb8fa09fae56307e0061fbd8f3d41eead069fe
AlanCoding <arominge@redhat.com>
2016-11-17 08:20:19 -0500

    add assertion to validate migration data creation

--- demo/rockband/tests/test_rockband_models.py
+++ demo/rockband/tests/test_rockband_models.py
@@ -5,3 +5,8 @@ from rockband.models import Band, Member
 def test_invalid_instrument_choice():
     band = Band.objects.create(name='RockSugar')
     Member.objects.create(band=band, instrument='z')
+
+@pytest.mark.django_db
+def test_auto_create_by_migration():
+    band = Band.objects.first()
+    assert band.name == 'Rock Sugar'
